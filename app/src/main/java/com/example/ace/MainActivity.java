64db26d1eee8f37c105a33368cd3ce3e790a30d5
connorES <connoresmith777@gmail.com>
2022-03-17 14:18:17 +1100

    test

--- app/src/main/java/com/example/ace/MainActivity.java
+++ app/src/main/java/com/example/ace/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(MainActivity.this, Register.class);
                 startActivity(intent);
+                System.out.print("branch test");
             }
         });
 
