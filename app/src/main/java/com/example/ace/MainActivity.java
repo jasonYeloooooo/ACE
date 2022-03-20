aa32155de8be2b9c3e42e95ab9a6a2a654b1b4c4
jasonYeloooooo <u3185939@canberra.edu.au>
2022-03-20 15:55:23 +1100

    firebase added ---jason

--- app/src/main/java/com/example/ace/MainActivity.java
+++ app/src/main/java/com/example/ace/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.ace;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private Button signinBtn;
@@ -35,5 +42,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
     }
 }
