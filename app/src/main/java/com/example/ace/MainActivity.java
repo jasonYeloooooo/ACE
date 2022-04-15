2aaeeefa3d1fe17d612a92ac7dbe9022f67587df
u3160557 <u3160557@uni.canberra.edu.au>
2022-04-16 00:08:11 +1000

    Revert "Dialogflow working demo"
    This reverts commit e45888b13497f6439707dfcfa13bebd015a208af.

--- app/src/main/java/com/example/ace/MainActivity.java
+++ app/src/main/java/com/example/ace/MainActivity.java
@@ -1,68 +1,24 @@
 package com.example.ace;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.SyncStateContract;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-import com.example.ace.adapters.ChatAdapter;
-import com.example.ace.helpers.SendMessageInBg;
-import com.example.ace.interfaces.BotReply;
-import com.example.ace.models.Message;
-import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.dialogflow.v2.DetectIntentResponse;
-import com.google.cloud.dialogflow.v2.QueryInput;
-import com.google.cloud.dialogflow.v2.SessionName;
-import com.google.cloud.dialogflow.v2.SessionsClient;
-import com.google.cloud.dialogflow.v2.SessionsSettings;
-import com.google.cloud.dialogflow.v2.TextInput;
-import com.google.common.collect.Lists;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 // connors branch
 
-public class MainActivity extends AppCompatActivity implements BotReply{
+public class MainActivity extends AppCompatActivity {
     private Button signinBtn;
     private Button registerBtn;
 
-    RecyclerView chatView;
-    ChatAdapter chatAdapter;
-    List<Message> messageList = new ArrayList<>();
-    EditText editMessage;
-    ImageButton btnSend;
-
-    //dialogFlow
-    private SessionsClient sessionsClient;
-    private SessionName sessionName;
-    private  String uuid = UUID.randomUUID().toString();
-    private String TAG = "mainactivity";
-
-  /*  @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        hideToolBr();
         //click the Login button, jump to the login page
         signinBtn = findViewById(R.id.signInBtn);
         signinBtn.setOnClickListener(new View.OnClickListener() {
@@ -83,129 +39,5 @@ public class MainActivity extends AppCompatActivity implements BotReply{
                 System.out.print("branch test");
             }
         });
-
     }
-*/
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        chatView = findViewById(R.id.chatView);
-        editMessage = findViewById(R.id.editMessage);
-        btnSend = findViewById(R.id.btnSend);
-
-
-        chatAdapter = new ChatAdapter(messageList, this);
-        chatView.setAdapter(chatAdapter);
-
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View view) {
-                String message = editMessage.getText().toString();
-                if (!message.isEmpty()){
-                    messageList.add(new Message(message,false));
-                    editMessage.setText("");
-                    sendMessageToBot(message);
-                    Objects.requireNonNull(chatView.getAdapter()).notifyDataSetChanged();
-                    Objects.requireNonNull(chatView.getLayoutManager())
-                            .scrollToPosition(messageList.size()-1);
-                }else{
-                    Toast.makeText(MainActivity.this, "Please enter text", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        setUpBot();
-
-    }
-
-    private void setUpBot(){
-        try {
-            InputStream stream = this.getResources().openRawResource(R.raw.credential);
-            GoogleCredentials credentials = GoogleCredentials.fromStream(stream)
-                    .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
-            String projectId =  ((ServiceAccountCredentials) credentials).getProjectId();
-
-            SessionsSettings.Builder settingsBuilder = SessionsSettings.newBuilder();
-            SessionsSettings sessionsSettings = settingsBuilder.setCredentialsProvider(
-                    FixedCredentialsProvider.create(credentials)).build();
-            sessionsClient = SessionsClient.create(sessionsSettings);
-            sessionName = SessionName.of(projectId, uuid);
-
-            Log.d(TAG, "projectId: " + projectId);
-
-
-        }catch (Exception e){
-            Log.d(TAG, "setUpBot: " + e.getMessage());
-        }
-    }
-
-    private void sendMessageToBot(String message) {
-        QueryInput input = QueryInput.newBuilder()
-                .setText(TextInput.newBuilder().setText(message).setLanguageCode("en-US")).build();
-
-        new SendMessageInBg(this, sessionName, sessionsClient, input).execute();
-
-    }
-
-    @Override
-    public void callback(DetectIntentResponse returnResponse) {
-        if(returnResponse != null){
-            String botReply = returnResponse.getQueryResult().getFulfillmentText();
-            if(!botReply.isEmpty()){
-                messageList.add(new Message(botReply, true));
-                chatAdapter.notifyDataSetChanged();
-                chatView.getLayoutManager().scrollToPosition(messageList.size()-1);
-            }else{
-                Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show();
-            }
-        }else{
-            Toast.makeText(this, "failed to connect", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-
-    public void hideToolBr(){
-
-        // BEGIN_INCLUDE (get_current_ui_flags)
-        // The UI options currently enabled are represented by a bitfield.
-        // getSystemUiVisibility() gives us that bitfield.
-        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
-        int newUiOptions = uiOptions;
-        // END_INCLUDE (get_current_ui_flags)
-        // BEGIN_INCLUDE (toggle_ui_flags)
-        boolean isImmersiveModeEnabled =
-                ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
-        if (isImmersiveModeEnabled) {
-            //Log.i(SyncStateContract.Constants.TAG_DEF, "Turning immersive mode mode off. ");
-        } else {
-          // Log.i(Constants.TAG_DEF, "Turning immersive mode mode on.");
-        }
-        // Navigation bar hiding:  Backwards compatible to ICS.
-        if (Build.VERSION.SDK_INT >= 14) {
-            newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        }
-        // Status bar hiding: Backwards compatible to Jellybean
-        if (Build.VERSION.SDK_INT >= 16) {
-            newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
-        }
-        // Immersive mode: Backward compatible to KitKat.
-        // Note that this flag doesn't do anything by itself, it only augments the behavior
-        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
-        // all three flags are being toggled together.
-        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
-        // Sticky immersive mode differs in that it makes the navigation and status bars
-        // semi-transparent, and the UI flag does not get cleared when the user interacts with
-        // the screen.
-        if (Build.VERSION.SDK_INT >= 18) {
-            newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        }
-        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
-        //END_INCLUDE (set_ui_flags)
-
-    }
-
-
 }
--- app/src/main/java/com/example/ace/interfaces/BotReply.java
+++
@@ -1,8 +0,0 @@
-package com.example.ace.interfaces;
-
-import com.google.cloud.dialogflow.v2.DetectIntentResponse;
-
-public interface BotReply {
-    public void callback(DetectIntentResponse returnResponse);
-
-}
